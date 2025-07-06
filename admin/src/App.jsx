3951ec736b140f1ef686e73f0e992c4389c23c0b
Uyi Obazee <utejoe.ju@gmail.com>
2025-07-06 11:55:34 +0100

    committing new changes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Admin from './Pages/Admin/Admin'
+// App.jsx
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Admin from "./Pages/Admin/Admin";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Admin />
+      <Routes>
+        <Route path="/*" element={<Admin />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
