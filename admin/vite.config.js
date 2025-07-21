f274b089d97951ec2d384866702ed84dc59e6cb1
Uyi Obazee <utejoe.ju@gmail.com>
2025-07-21 23:41:50 +0100

    Remove upload folder from Git tracking

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,7 +1,11 @@
+// admin/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173
+  }
 })
