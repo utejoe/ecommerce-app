c529ef920a7751fe04f4ec9ed4b266aff56fa3b5
Uyi Obazee <utejoe.ju@gmail.com>
2025-07-21 23:46:00 +0100

    Remove upload folder from Git tracking

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -6,6 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 5173
-  }
+    port: parseInt(process.env.PORT) || 5173,
+  },
+  preview: {
+    port: parseInt(process.env.PORT) || 4173,
+    host: '0.0.0.0',
+    allowedHosts: ['ecommerce-admin-lzar.onrender.com'],
+  },
 })
